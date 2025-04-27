c5c7dee622cc651670058402c10e6976b92b2372
gui69961 <gui.palmeiras27@gmail.com>
2025-04-27 13:10:44 -0300

    att

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const app = express();
 
-
+// Definindo a porta 80
 const PORT = 80;
 
+// Deixa a pasta "public" acessível
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Inicia o servidor
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando em http://<IP da máquina>`);
 });
-=======
-require("colors");
-
-var http = require("http");
-
-var express = require("express");
-
-var app = express();
-
-app.use(express.static('./public'));
-
-var server = http.createServer(app);
-
-server.listen(80);
-
-console.log("servidor rodando...");
-
-console.log("Servidor rodando".rainbow);
->>>>>>> 1c4ae3ca15d88a6fd63a786e14728000f199f905
 
