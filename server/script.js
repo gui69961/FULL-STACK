4b7274f53c68afc8e8eacf95b7668aa5c2cfac32
gui69961 <gui.palmeiras27@gmail.com>
2025-04-28 21:27:40 -0300

    att

---
+++ server/script.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log('Servidor rodando ...'.rainbow);
+
+
+
+
+app.get('/cadastro',function (requisicao, resposta){
+let nome = requisicao.query.nome
+let email = requisicao.query.email
+let nascimento= requisicao.query.nascimento
+
+
+
+
+resposta.render('resposta.ejs', {mensagem: "Usuário cadastrado com sucesso!", usuario: nome, email: email})
+
+})
+
+app.post('/cadastro',function (requisicao, resposta){
+    let nome = requisicao.body.nome
+    let email = requisicao.body.email
+    let nascimento= requisicao.body.nascimento
+  
+
+    resposta.render('resposta.ejs')
+    
+    })
